ec995566a9796962d3be71f3b0b420d909ca25a6
Hemachandru J <hemachandru412@gmail.com>
2022-09-27 18:08:13 +0530

    xunit Tests completed

--- GroceryAPI.Tests/Controller/AccountControllerTests.cs
+++ GroceryAPI.Tests/Controller/AccountControllerTests.cs
@@ -11,9 +11,7 @@ namespace GroceryAPI.Tests.Controller
 {
     public class AccountControllerTests
     {
-        
         private readonly IConfiguration _configuration;
-
 
         [Fact]
         public async Task AdminLogin_Test()
@@ -37,7 +35,6 @@ namespace GroceryAPI.Tests.Controller
             result.Should().NotBeNull();
             result.Should().BeOfType<ActionResult<Admin>>();
             result.Value.Should().BeEquivalentTo(admin);
-
         }
 
         [Fact]
@@ -115,6 +112,53 @@ namespace GroceryAPI.Tests.Controller
             result.Should().BeOfType<ActionResult<JWTToken>>();
             result.Value.customer.CustomerEmail.Should().Be(customer1.CustomerEmail);
             dbContext.customer.Should().HaveCount(5);
+            result.Value.Token.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public async Task CustomerLogin_ReturnNull()
+        {
+            //Arrange
+            var inMemory = new InMemoryDbContext();
+            var dbContext = await inMemory.GetDatabaseContext();
+            var myConfiguration = new Dictionary<string, string>
+            {
+                {"JWT:ValidAudience", "User"},
+                {"JWT:ValidIssuer", "http://localhost:55126"},
+                {"JWT:Secret", "hJWTAuthenticationHIGHsecuredPasswordVVVp1OH7Xzyr"}
+            };
+
+            var _configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(myConfiguration)
+                .Build();
+            var accountController = new AccountController(dbContext, _configuration);
+            Customer customer = new Customer()
+            {
+                //CustomerID = 15,
+                CustomerName = "Chandru",
+                CustomerEmail = "Chandru@gmail.com",
+                MobileNumber = 985632587,
+                Address = "Chennai",
+                CartTypeId = null,
+                Password = "!Morning1",
+            };
+            Customer customer1 = new Customer()
+            {
+                CustomerID = 18,
+                CustomerName = "Chandru",
+                CustomerEmail = "Chandru1@gmail.com",
+                MobileNumber = 985632587,
+                Address = "Chennai",
+                CartTypeId = null,
+                Password = "!Morning1",
+            };
+
+            //Act
+            await accountController.Register(customer);
+            var result = await accountController.CustomerLogin(customer1);
+
+            //Assert
+            result.Should().BeNull();
         }
 
         [Fact]
